0b4ce7a0d1ad4c63edfc47cc6145c08502aa3d81
josh stilwell <josh@MacBooks-MacBook-Pro.local>
2017-07-26 17:51:08 -0400

    Add form to publish a new post
    New posts are rendered onto homepage in order by most recent post. Form for publishing has server-side validation.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,16 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
-    public function index() //controller action
+    public function index() // controller action
     {
-    	return view('posts.index');
+    	$posts = Post::latest()->get(); // order posts by newest
+    	return view('posts.index', compact('posts'));
     }
 
-    public function show() 
+    public function show(Post $post) 
     {
-    	return view('posts.show');
+    	return view('posts.show', compact('post'));
+    }
+
+    public function create() 
+    {
+    	return view('posts.create');
+    }
+
+    public function store() 
+    {
+    	// $post = new Post;
+
+    	// $post->title = request('title');
+    	// $post->body = request('body');
+
+    	// $post->save();
+
+    	$this->validate(request(), [
+    		'title' => 'required',
+    		'body' => 'required'
+    		]);
+
+    	Post::create(request(['title', 'body']));
+
+    	return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts', 'PostsController@show');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/create', 'PostsController@create');
+Route::get('/posts/{post}', 'PostsController@show');
