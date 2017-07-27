6c5d8c487f76232ff9d50b33fb536c9475f8a813
josh stilwell <josh@MacBooks-MacBook-Pro.local>
2017-07-27 11:09:13 -0400

    Configure Laravel Mix for the front-end

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 	<!-- CUSTOM TEMPLATE STYLES -->
-	<link href="/css/blog.css" rel="stylesheet">
+	<link href="/css/app.css" rel="stylesheet">
 	<title>Josh Stilwell</title>
 </head>
 	@include ('layout.nav')
